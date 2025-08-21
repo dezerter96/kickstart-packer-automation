965ef235a9b59749f79251725aaf4574db6d94ab
Szymon Reclaf <szymon.reclaf@hotmail.com>
2025-08-21 14:40:35 +0000

    Updated password_generator.py

--- scripts/password_generator.py
+++ scripts/password_generator.py
@@ -1,7 +1,7 @@
 import crypt
 
-password = input("Podaj hasło: ")
+password = input("Enter your password: ")
 hashed_password = crypt.crypt(password, "$6$rounds=656000$" + crypt.mksalt(crypt.METHOD_SHA512)[3:])
 
-print("Zahashowane hasło:")
+print("Hashed password: ")
 print(hashed_password)
